03562252175be38ac90aed08726c6b577d7c34f6
Aria Lee <74793453+arialee32708@users.noreply.github.com>
2022-07-13 09:45:06 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,4 +41,5 @@ streamlit.write('The user entered ', fruit_choice)
 import requests
 fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
+#import connector
 import snowflake.connector
