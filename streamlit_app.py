72105caddaf2e6997a4a994dd8558f58d723043e
Aria Lee <74793453+arialee32708@users.noreply.github.com>
2022-07-13 09:47:48 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,3 +43,6 @@ fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + fruit_cho
 
 #import connector
 import snowflake.connector
+snowflake-connector-python
+cffi==1.15.0
+pyarrow==6.0.1
