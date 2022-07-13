bc3ab83c4813531f73972f8647e0f152967e0c53
Aria Lee <74793453+arialee32708@users.noreply.github.com>
2022-07-13 10:16:23 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,3 +46,11 @@ fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + fruit_cho
 snowflake-connector-python
 cffi==1.15.0
 pyarrow==6.0.1
+
+import snowflake.connector
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
