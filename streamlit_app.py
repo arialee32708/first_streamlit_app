7709fb89b1a59c10674f1aab249a0e4e8bf079ed
Aria Lee <74793453+arialee32708@users.noreply.github.com>
2022-07-14 11:05:19 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,10 +37,13 @@ streamlit.dataframe(fruits_to_show)
 #new section to display fruityvice api response
 import requests
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?')
+fruit_choice = streamlit.text_input('What fruit would you like information about?'+ ' ')
 streamlit.write('The user entered ', fruit_choice)
 import requests
-fruityvice_response=requests.get("https://fruityvice.com/api/fruit/all" + fruit_choice)
+fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+streamlit.text(fruityvice_response.json())
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalized)
 
 #import connector
 #import snowflake.connector
