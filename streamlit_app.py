c480f9df92e0d68600d1fe4fe847932bf75fc416
Aria Lee <74793453+arialee32708@users.noreply.github.com>
2022-07-15 10:03:04 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,23 +27,23 @@ streamlit.dataframe(fruits_to_show)
 
 
 #import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
-streamlit.text(fruityvice_response.json()) #just writes the data to the screen 
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+#streamlit.text(fruityvice_response.json()) #just writes the data to the screen 
 #take the json version of the response and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #output it the screen as a table
-streamlit.dataframe(fruityvice_normalized)
+#streamlit.dataframe(fruityvice_normalized)
 
 #new section to display fruityvice api response
-import requests
-streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?'+ "kiwi")
-streamlit.write('The user entered ', fruit_choice)
-import requests
-fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+#import requests
+#streamlit.header('Fruityvice Fruit Advice!')
+#fruit_choice = streamlit.text_input('What fruit would you like information about?'+ "kiwi")
+#streamlit.write('The user entered ', fruit_choice)
+#import requests
+#fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #streamlit.text(fruityvice_response.json())
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
+#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#streamlit.dataframe(fruityvice_normalized)
 
 #import connector
 #import snowflake.connector
